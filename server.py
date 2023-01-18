eff10e067f2f37af4ca05414ed7dd4ddf5de44c1
bkoj-arch <loupfourment@gmail.com>
2023-01-18 14:20:38 +0100

    ajout server socket

---
+++ server.py
@@ -0,0 +1,5 @@
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# bind the socket to a public host, and a well-known port
+serversocket.bind((socket.gethostname(), 80))
+# become a server socket
+serversocket.listen(5)
