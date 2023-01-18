054b40d2c089b20a65eb85f43dc67f76c7ccd20a
zouineoualid <zouineoualid@gmail.com>
2023-01-18 11:43:19 +0100

    ajout oualid.py

---
+++ oualid.py
@@ -0,0 +1,14 @@
+import os
+a=1
+while True:
+    print ("oualid test")
+    if a == 1:
+        print("Un")
+        break
+    elif a == 4:
+        print("quatre")
+        break
+    else :
+        print ("test ") 
+        break      
+print ("sortie de ma boucle")
